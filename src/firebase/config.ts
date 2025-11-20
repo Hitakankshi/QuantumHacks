4cfa220c9c6f9420a8f2970c77a4bdfa954d6e29
Hitakankshi Behera <hitakankshib@gmail.com>
2025-11-20 20:36:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2383970766-fc9aa",
+  "appId": "1:875597201698:web:dd2b2beac092c63beba57b",
+  "apiKey": "AIzaSyBJuZcMQUqmnk8PnceYnYOMDgQCB0lnADE",
+  "authDomain": "studio-2383970766-fc9aa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "875597201698"
+};
